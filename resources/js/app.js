c7bfecc171ce7ce16e833c245fd43ecab75c460d
takayuki-miyazaki-adglobe-co-jp <takayuki.miyazaki@adglobe.co.jp>
2022-09-16 12:26:07 +0900

    feat: index（welcome） の要素を Vue コンポーネントに変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,9 @@ import { createApp } from 'vue';
 const app = createApp({});
 
 import ExampleComponent from './components/ExampleComponent.vue';
+import LaravelTop from './components/LaravelTop.vue';
 app.component('example-component', ExampleComponent);
+app.component('laravel-top', LaravelTop);
 
 /**
  * The following block of code may be used to automatically register your
